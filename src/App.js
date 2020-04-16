7584c2377fbbf74d7148e5276df4878008805087
Jacob Ashley <jacobashley@Jacobs-Mac-Pro.local>
2020-04-16 11:30:50 -0700

    commit 4.16

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Main from './Main/Main'
 import FolderMain from './Main/FolderMain'
 import NoteMain from './Main/NoteMain'
 import STORE from './STORE'
+import Context from './Context'
 
 export default class App extends Component {
   state = {
@@ -18,111 +19,121 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({...STORE, selectedNotes: STORE.notes} )
+    this.setState({ ...STORE, selectedNotes: STORE.notes })
   }
 
-  setSelectedFolder = name => this.setState(state => { 
-    const {id} = state.folders.find(folder => folder.name === name)
+  setSelectedFolder = name => this.setState(state => {
+    const { id } = state.folders.find(folder => folder.name === name)
     return {
-      selectedFolder: name, 
-      selectedNotes: state.notes.filter(note => note.folderId === id) 
+      selectedFolder: name,
+      selectedNotes: state.notes.filter(note => note.folderId === id)
     }
   })
 
-  goHome = () => this.setState( state => ({
+  goHome = () => this.setState(state => ({
     selectedFolder: "",
     selectedNotes: state.notes
   }))
 
   render() {
+    const value = {
+      notes: this.state.notes,
+      folders: this.state.folders
+    }
+
     return (
-      <div className='App'>
+      <Context.Provider value={value}>
+        <div className='App'>
 
-        <header className='header'>
-          <Link to="/" onClick={this.goHome}><h1>Noteful</h1></Link>{' '}
-        </header>
+          <header className='header'>
+            <Link to="/" onClick={this.goHome}><h1>Noteful</h1></Link>{' '}
+          </header>
 
-        <div className='content'>
+          <div className='content'>
 
-          <div className='Sidebar'>
-            <Switch>
-              <Route
-                exact path='/'
-                render={(routeProps) =>
-                  <MainSidebar
-                    setSelectedFolder={this.setSelectedFolder}
-                    selectedFolder={this.state.selectedFolder}
-                    folders={this.state.folders}
-                    {...routeProps}
-                  />
-                }
-              />
+            <div className='Sidebar'>
+              <Switch>
+                <Route
+                  exact path='/'
+                  setSelectedFolder={this.setSelectedFolder}
+                  selectedFolder={this.state.selectedFolder}
+                  component={MainSidebar}
+                  // render={(routeProps) =>
+                  //   <MainSidebar
+                  //     setSelectedFolder={this.setSelectedFolder}
+                  //     selectedFolder={this.state.selectedFolder}
+                  //     folders={this.state.folders}
+                  //     {...routeProps}
+                  //   />
+                  // }
+                />
 
-              <Route
-                path='/folder/:folderId'
-                render={(routeProps) =>
-                  <FolderSidebar
-                    setSelectedFolder={this.setSelectedFolder}
-                    selectedFolder={this.state.selectedFolder}
-                    folders={this.state.folders}
-                    {...routeProps}
-                  />
-                }
-              />
+                <Route
+                  path='/folder/:folderId'
+                  render={(routeProps) =>
+                    <FolderSidebar
+                      setSelectedFolder={this.setSelectedFolder}
+                      selectedFolder={this.state.selectedFolder}
+                      folders={this.state.folders}
+                      {...routeProps}
+                    />
+                  }
+                />
 
-              <Route
-                path='/note/:noteId'
-                render={(routeProps) =>
-                  <NoteSidebar
-                    folders={this.state.folders}
-                    {...routeProps}
-                  />
-                }
-              />
-            </Switch>
-
-
-          </div>
-
-          <div className="mainMain">
-            <Switch>
-              <Route
-                exact path='/'
-                render={(routeProps) =>
-                  <Main
-                    notes={this.state.selectedNotes}
-                    {...routeProps}
-                  />
-                }
-              />
-
-              <Route
-                path='/folder/:folderId'
-                render={(routeProps) =>
-                  <FolderMain
-                    notes={this.state.selectedNotes}
-                    folders={this.state.folders}
-                    {...routeProps}
-                  />
-                }
-              />
-
-              <Route
-                path='/note/:noteId'
-                render={(routeProps) =>
-                  <NoteMain
-                    folders={this.state.folders}
-                    notes={this.state.selectedNotes}
-                    {...routeProps}
-                  />
-                }
-              />
-            </Switch>
+                <Route
+                  path='/note/:noteId'
+                  render={(routeProps) =>
+                    <NoteSidebar
+                      folders={this.state.folders}
+                      {...routeProps}
+                    />
+                  }
+                />
+              </Switch>
 
 
+            </div>
+
+            <div className="mainMain">
+              <Switch>
+                <Route
+                  exact path='/'
+                  render={(routeProps) =>
+                    <Main
+                      notes={this.state.selectedNotes}
+                      {...routeProps}
+                    />
+                  }
+                />
+
+                <Route
+                  path='/folder/:folderId'
+                  render={(routeProps) =>
+                    <FolderMain
+                      notes={this.state.selectedNotes}
+                      folders={this.state.folders}
+                      {...routeProps}
+                    />
+                  }
+                />
+
+                <Route
+                  path='/note/:noteId'
+                  render={(routeProps) =>
+                    <NoteMain
+                      folders={this.state.folders}
+                      notes={this.state.selectedNotes}
+                      {...routeProps}
+                    />
+                  }
+                />
+              </Switch>
+
+
+            </div>
           </div>
         </div>
-      </div>
+      </Context.Provider>
     )
   }
 }
--- src/Sidebar/MainSidebar.js
+++ src/Sidebar/MainSidebar.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Folders from './Folders'
 import './Sidebar.css'
+import Context from '../Context'
 
-export default function MainSidebar(props) {
-    return (
-        <div className='sidebar__container'>
-            <div className="mainButtons">
-                <ul>
-                    { props.folders.map((folderName) => {
-                        return <Folders folderName= { folderName } key={folderName.id} setSelectedFolder={props.setSelectedFolder} selectedFolder={props.selectedFolder}/>
-                    })}
-                </ul>
-            </div>
-            <div className='addFolderButton'>
-                <button className='button'>Add Folder</button>
-            </div>
-        </div>
-    )
+export default class MainSidebar extends Component {
+    static contextType = Context;
+    render() {
+        return (
+            <Context.Consumer>
+                <div className='sidebar__container'>
+                    <div className="mainButtons">
+                        <ul>
+                            {this.context.folders.map((folderName) => {
+                                return <Folders 
+                                    folderName={folderName} 
+                                    key={folderName.id} 
+                                    setSelectedFolder={this.props.setSelectedFolder} 
+                                    selectedFolder={this.props.selectedFolder} 
+                                />
+                            })}
+                        </ul>
+                    </div>
+                    <div className='addFolderButton'>
+                        <button className='button'>Add Folder</button>
+                    </div>
+                </div>
+            </Context.Consumer>
+        )
+    }
 }
