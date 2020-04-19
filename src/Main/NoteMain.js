6edef0615fb2a7073894763a2307ef964f9b262c
Jacob Ashley <jacobashley@Jacobs-Mac-Pro.local>
2020-04-19 13:31:01 -0700

    notefix

--- src/Main/NoteMain.js
+++ src/Main/NoteMain.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Main.css'
 import Context from '../Context'
+import Note from './Note'
 
 export default class NoteMain extends Component {
     static defaultProps = {
@@ -17,16 +18,7 @@ export default class NoteMain extends Component {
         )
         return (
             <>
-                <div className='note'>
-
-                    <h3>{note && note.name}</h3>
-
-                    <p>Date modified on </p> {note && note.modified}
-                    <button 
-                        className='deleteButton'
-                        
-                    >Delete Note</button>
-                </div>
+                <Note/>
                 <div className="contentContainer">
                     <p>{note && note.content}</p>
                 </div>
